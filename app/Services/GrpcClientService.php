e34096d2e0bf74efbe5b756dfeb3b187e2fceaea
escanor <marouan.safsafi@student.uhasselt.be>
2023-12-23 22:15:16 +0100

    aanpassing grpc, maar steeds een probleem

--- app/Services/GrpcClientService.php
+++ app/Services/GrpcClientService.php
@@ -3,25 +3,48 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+use App\Generated\MaintenanceServiceClient;
+use App\Generated\AppointmentRequest;
+use Spiral\RoadRunner\GRPC;
+
 
 
 class GrpcClientService
 {
     protected $baseUrl;
+    protected $client;
+
 
     public function __construct()
     {
-        $this->baseUrl = 'http://localhost:50051'; // Pas dit aan naar de URL van je Go gRPC-service
+        //$this->baseUrl = 'http://localhost:50051'; // Pas dit aan naar de URL van je Go gRPC-service
+        $this->client = new MaintenanceServiceClient('localhost:50051', [
+            'credentials' => GRPC\ChannelCredentials::createInsecure(),
+        ]);
     }
 
+    // public function scheduleAppointment($userId, $taskDescription, $preferredTime)
+    // {
+    //     $response = Http::post("{$this->baseUrl}/schedule-appointment", [
+    //         'user_id' => $userId,
+    //         'task_description' => $taskDescription,
+    //         'preferred_time' => $preferredTime,
+    //     ]);
+
+    //     return $response->json();
+    // }
     public function scheduleAppointment($userId, $taskDescription, $preferredTime)
     {
-        $response = Http::post("{$this->baseUrl}/schedule-appointment", [
-            'user_id' => $userId,
-            'task_description' => $taskDescription,
-            'preferred_time' => $preferredTime,
-        ]);
+        $request = new AppointmentRequest();
+        $request->setUserId($userId);
+        $request->setTaskDescription($taskDescription);
+        $request->setPreferredTime($preferredTime);
 
-        return $response->json();
+        list($response, $status) = $this->client->ScheduleAppointment($request)->wait();
+        if ($status->code !== GRPC\STATUS_OK) {
+            throw new \Exception("gRPC request failed: " . $status->details);
+        }
+
+        return $response;
     }
 }
