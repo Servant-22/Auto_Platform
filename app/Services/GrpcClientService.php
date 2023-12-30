6677ceb831c70705ff13ae9ba4829610a473feae
escanor <marouan.safsafi@student.uhasselt.be>
2023-12-30 16:18:53 +0100

    to bad

--- app/Services/GrpcClientService.php
+++ app/Services/GrpcClientService.php
@@ -4,7 +4,6 @@ namespace App\Services;
 use Grpc\ChannelCredentials;
 use Maintenance\MaintenanceServiceClient;
 use Maintenance\AppointmentRequest;
-use Spiral\GRPC\Context;
 use Illuminate\Support\Facades\Http;
 
 class GrpcClientService
@@ -14,43 +13,42 @@ class GrpcClientService
 
     public function __construct()
     {
-        // $this->baseUrl = 'http://localhost:50051';
+        $this->baseUrl = 'http://localhost:50051';
         
-        $this->client = new MaintenanceServiceClient('localhost:50051', [
-            'credentials' => \Grpc\ChannelCredentials::createInsecure(),
-        ]);
+        // $this->client = new MaintenanceServiceClient('localhost:50051', [
+        //     'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+        // ]);
         
     }
 
-    public function scheduleAppointment($userId, $taskDescription, $preferredTime)
-    {
-        $request = new AppointmentRequest();
-        $request->setUserId($userId);
-        $request->setTaskDescription($taskDescription);
-        $request->setPreferredTime($preferredTime);
+    // public function scheduleAppointment($userId, $taskDescription, $preferredTime)
+    // {
+    //     $request = new AppointmentRequest();
+    //     $request->setUserId($userId);
+    //     $request->setTaskDescription($taskDescription);
+    //     $request->setPreferredTime($preferredTime);
 
-        list($response, $status) = $this->client->ScheduleAppointment($request)->wait();
-        if ($status->code !== \Grpc\STATUS_OK) {
-            throw new \Exception("gRPC request failed: " . $status->code);
-        }
-        dd($response);
+    //     list($response, $status) = $this->client->ScheduleAppointment($request)->wait();
+    //     if ($status->code !== \Grpc\STATUS_OK) {
+    //         throw new \Exception("gRPC request failed: " . $status->code);
+    //     }
 
-        return $response;
-    }
+    //     return $response;
+    // }
 
     // public function __destruct()
     // {
     //     $this->client->close();
     // }
 
-       // public function scheduleAppointment($userId, $taskDescription, $preferredTime)
-    // {
-    //     $response = Http::post("{$this->baseUrl}/schedule-appointment", [
-    //         'user_id' => $userId,
-    //         'task_description' => $taskDescription,
-    //         'preferred_time' => $preferredTime,
-    //     ]);
+       public function scheduleAppointment($userId, $taskDescription, $preferredTime)
+    {
+        $response = Http::post("{$this->baseUrl}/schedule-appointment", [
+            'user_id' => $userId,
+            'task_description' => $taskDescription,
+            'preferred_time' => $preferredTime,
+        ]);
 
-    //     return $response->json();
-    // }
+        return $response->json();
+    }
 }
