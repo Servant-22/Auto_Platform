a51a03cce60bf72fe790f60a6b90994b1d62dc84
escanor <marouan.safsafi@student.uhasselt.be>
2024-01-02 11:46:10 +0100

    beetje front end

--- resources/views/feedback_thank_you.blade.php
+++ resources/views/feedback_thank_you.blade.php
@@ -1,12 +1,18 @@
 {{-- resources/views/feedback_thank_you.blade.php --}}
-
+<link rel="stylesheet" href="{{ asset('css/feedback_thank_you.css') }}">
 @extends('layouts.app')
 
 @section('content')
-    <h1>Bedankt voor uw feedback!</h1>
-    <p>Uw mening is belangrijk voor ons.</p>
+    <div class="card">
+        <h1>Bedankt voor uw feedback!</h1>
+        <p>Uw mening is belangrijk voor ons.</p>
 
-    @if(isset($averageScore))
-        <p>De huidige gemiddelde score is: {{ $averageScore }}</p>
-    @endif
+        @if(isset($averageScore))
+            <p>De huidige gemiddelde score is: {{ $averageScore }}</p>
+        @endif
+    </div>
 @endsection
+
+
+
+
